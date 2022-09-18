e8cf207c6aff6e022d1a39a241e10d8e94f93cbf
tonito45 <somusartungi47@gmail.com>
2022-09-18 10:32:02 +0545

    new commit

--- asset/script.js
+++ asset/script.js
@@ -1,7 +1,23 @@
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
+    ShowModal();
   }
 
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+
+  const modal = document.querySelector("#modal");
+
+  const ShowModal =()=> {
+      // modal.classList.toggle("overlay");
+      let body = document.getElementById("body");
+      body.style.overflow = "hidden";
+      // if (!modal.classList.contains("overlay")) {
+      //     // Disable scroll
+      //     body.style.overflow = "hidden";
+      // } else {
+      //     // Enable scroll
+      //     body.style.overflow = "auto";
+      // }
+  };
