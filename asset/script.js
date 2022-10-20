95752becaa252f7494c5911bd120140bc6e7c8df
tonito45 <somusartungi47@gmail.com>
2022-10-20 17:32:25 +0545

    collapsible

--- asset/script.js
+++ asset/script.js
@@ -6,3 +6,14 @@ function openNav() {
     document.getElementById("myNav").style.width = "0%";
   }
 
+
+  function backOpen(x){
+    let heightBox=document.querySelector(x);
+   if(heightBox.style.height==="40rem"){
+    heightBox.style.height="20rem";
+   }else{
+    heightBox.style.height="40rem";
+   }
+}
+
+
